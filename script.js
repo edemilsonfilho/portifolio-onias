0863315ad8495164d8dab59130cec1149d77f770
Edemilson Filho <edemilsonr62@gmail.com>
2025-02-23 17:10:49 -0400

    ajuste expansao do menu

--- script.js
+++ script.js
@@ -5,29 +5,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
     menuToggle.addEventListener("click", function () {
         menuOverlay.classList.add("active");
+        menuToggle.classList.add("active");
     });
 
     closeBtn.addEventListener("click", function () {
         menuOverlay.classList.remove("active");
+        menuToggle.classList.remove("active");
     });
 
     menuOverlay.addEventListener("click", function (e) {
         if (e.target === menuOverlay) {
             menuOverlay.classList.remove("active");
+            menuToggle.classList.remove("active");
         }
     });
-});
 
-document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll(".menu a");
+    const links = document.querySelectorAll(".menu-items a");
 
     links.forEach(link => {
         link.addEventListener("click", function (e) {
-            e.preventDefault();
             const targetId = this.getAttribute("href").substring(1);
             const targetSection = document.getElementById(targetId);
 
             if (targetSection) {
+                e.preventDefault();
                 window.scrollTo({
                     top: targetSection.offsetTop - 50,
                     behavior: "smooth"
